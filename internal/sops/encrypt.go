b6cccbe8ccb34464d7ad5da684b2acd797f457e4
Aniruddha Sinha <asinha0493@gmail.com>
2026-08-25 23:05:03 +0530

    migrating to go v1.27

--- internal/sops/encrypt.go
+++ internal/sops/encrypt.go
@@ -20,7 +20,7 @@ var (
 )
 
 func Encrypt(inFile, format, keyType, keySpec string) (string, error) {
-	payload, err := os.ReadFile(inFile)
+	payload, err := os.ReadFile(inFile) //nolint:gosec // G304: File path is intended to be dynamically passed via CLI input variable
 	if err != nil {
 		return "", fmt.Errorf("%w, %w", ErrInFileReadError, err)
 	}
