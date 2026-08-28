91d345155ad0b693b952fcbfb55706d9bf09f0fb
Aniruddha Sinha <asinha0493@gmail.com>
2026-08-28 19:34:06 +0530

    removed the boilerplate and changed it to sops/v3/decrypt

--- internal/sops/decrypt.go
+++ internal/sops/decrypt.go
@@ -4,20 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/getsops/sops/v3"
-	"github.com/getsops/sops/v3/aes"
-	"github.com/getsops/sops/v3/keyservice"
+	"github.com/getsops/sops/v3/cmd/sops/formats"
+	"github.com/getsops/sops/v3/decrypt"
 )
 
 var (
-	ErrEncryptedFileRead  = errors.New("failed to parse encrypted file")
-	ErrRetrievingDataKey  = errors.New("error getting data key")
-	ErrDecryptingTree     = errors.New("error decrypting sops tree")
-	ErrFormattingPlainOut = errors.New("failed to format plaintext out")
-	ErrMACDecrypt         = errors.New("failed to decrypt MAC")
-	ErrMACMismatch        = errors.New("MAC mismatch: failed to verify data integrity")
+	ErrFormattingPlainOut   = errors.New("failed to format plaintext out")
+	ErrDecryptFailed        = errors.New("failed to decrypt sops encrypted data")
+	ErrParsingDecryptedData = errors.New("error parsing decrypted data for format conversion")
 )
 
 func Decrypt(inFormat, outFormat, inFile string) (string, error) {
@@ -26,93 +21,35 @@ func Decrypt(inFormat, outFormat, inFile string) (string, error) {
 		return "", fmt.Errorf("%w: %w", ErrInFileReadError, err)
 	}
 
-	tree, err := loadEncryptedFile(payload, inFormat)
+	inFmt := formats.FormatFromString(inFormat)
+	plaintext, err := decrypt.DataWithFormat(payload, inFmt)
+	if err != nil {
+		return "", fmt.Errorf("%w: %w", ErrDecryptFailed, err)
+	}
+
+	if inFormat == outFormat {
+		return string(plaintext), nil
+	}
+
+	inStore, err := ValidateFormatAndGetStore(inFormat)
 	if err != nil {
 		return "", err
 	}
 
-	keyServiceClient := keyservice.NewLocalClient()
-	dataKey, err := getDataKey(&keyServiceClient, tree)
+	branches, err := inStore.LoadPlainFile(plaintext)
 	if err != nil {
-		return "", err
-	}
-
-	result, err := decryptAndEmit(outFormat, dataKey, tree)
-	if err != nil {
-		return "", err
-	}
-
-	return string(result), nil
-}
-
-func loadEncryptedFile(payload []byte, format string) (*sops.Tree, error) {
-	store, err := ValidateFormatAndGetStore(format)
-	if err != nil {
-		return nil, err
-	}
-
-	tree, err := store.LoadEncryptedFile(payload)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrEncryptedFileRead, err)
-	}
-
-	return &tree, nil
-}
-
-func getDataKey(ksClient *keyservice.LocalClient, tree *sops.Tree) (*[]byte, error) {
-	dataKey, err := tree.Metadata.GetDataKeyWithKeyServices(
-		[]keyservice.KeyServiceClient{
-			ksClient,
-		}, nil)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrRetrievingDataKey, err)
-	}
-
-	return &dataKey, nil
-}
-
-func decryptAndEmit(outFormat string, dataKey *[]byte, tree *sops.Tree) ([]byte, error) {
-	cipher := aes.NewCipher()
-	if err := integrityCheck(tree, dataKey, cipher); err != nil {
-		return nil, err
+		return "", fmt.Errorf("%w: %w", ErrParsingDecryptedData, err)
 	}
 
 	outStore, err := ValidateFormatAndGetStore(outFormat)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	result, err := outStore.EmitPlainFile(tree.Branches)
+	result, err := outStore.EmitPlainFile(branches)
 	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrFormattingPlainOut, err)
+		return "", fmt.Errorf("%w: %w", ErrFormattingPlainOut, err)
 	}
 
-	return result, nil
-}
-
-/** @note
-** this is to prevent secret tampering where we calculate the MAC while decryption
-** and we compare the computed MAC with the original MAC which came packaged with the
-** encrypted data
- */
-func integrityCheck(tree *sops.Tree, dataKey *[]byte, cipher aes.Cipher) error {
-	computedMAC, err := tree.Decrypt(*dataKey, cipher)
-	if err != nil {
-		return fmt.Errorf("%w: %w", ErrDecryptingTree, err)
-	}
-
-	originalMAC, err := cipher.Decrypt(
-		tree.Metadata.MessageAuthenticationCode,
-		*dataKey,
-		tree.Metadata.LastModified.Format(time.RFC3339),
-	)
-	if err != nil {
-		return fmt.Errorf("%w: %w", ErrMACDecrypt, err)
-	}
-
-	if originalMAC != computedMAC {
-		return ErrMACMismatch
-	}
-
-	return nil
+	return string(result), nil
 }
